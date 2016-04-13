0306690d1b612b256ff85cb78449795f918aad3f
Nick Parker <nicholascparker@yahoo.com>
2016-04-12 23:44:07 -0400

    Enable users to place orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  
   devise_for :users
-  resources :listings
+  resources :listings do
+    resources :orders
+end
+
+
+
   get 'page/contact'
 
   get 'pages/contact'
